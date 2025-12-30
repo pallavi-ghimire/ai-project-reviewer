458b0fb66fcc292fedeb25fcf40719862ac03340
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2025-12-31 00:21:43 +0100

    updated readme and config

--- configs/config.py
+++ configs/config.py
@@ -1,6 +1,9 @@
 OLLAMA_URL = "http://localhost:11434/api/generate"
 MODEL_NAME = "gemma3:4b"
 
+#####################################
+# CODE EXTENSIONS OF FILES TO REVIEW
+#####################################
 # CODE_EXTENSIONS = {
 #     ".py", ".js", ".ts", ".tsx", ".java", ".go", ".rb", ".php", ".cs", ".cpp", ".c",
 #     ".rs", ".kt", ".swift", ".sql", ".html", ".css", ".md"
@@ -10,4 +13,7 @@ CODE_EXTENSIONS = {
     ".html", ".py", ".md"
 }
 
+##########################################
+# WHICH DIRECTORIES TO SKIP DURING REVIEW
+##########################################
 SKIP_DIRS = {".git", ".venv", "venv", "__pycache__", "node_modules", "dist", "build", ".idea", ".pytest_cache"}
